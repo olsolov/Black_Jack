77ebede86594d5fd294972fa3085e2e4b7eb4160
olgasolov <solo_o_e@mail.ru>
2019-06-25 13:22:59 +0300

    Add class Deck + def init, create_deck, take_card

---
+++ deck.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Deck
+  attr_reader :took_card
+
+  def initialize
+    @cards = []
+    @card_value = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+    @suits = ["\u2665".encode('utf-8'), "\u2666".encode('utf-8'), "\u2663".encode('utf-8'), "\u2660".encode('utf-8')]
+    create_deck
+  end
+
+  def create_deck
+    @suits.each do |suits|
+      @card_value.each do |value|
+        @cards << value + suits
+      end
+    end
+  end
+
+  def take_card
+    @took_card = @cards[rand(0..@cards.size)]
+    @cards.delete(@took_card)
+  end
+end

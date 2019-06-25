4aed46c488348faead626f718ff0e153a0de8c20
olgasolov <solo_o_e@mail.ru>
2019-06-25 15:53:34 +0300

    Add start for class Game

--- game.rb
+++ game.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
+require_relative 'player'
+require_relative 'bank'
+
 class Game
   print 'Введите ваше имя: '
   name = gets.strip.capitalize
+
+  def start
+    player = Player.new
+    bank_player = Bank.new(100)
+    player.add_bank(bank_player)
+  end
 end
+
+new_game = Game.new
+new_game.start

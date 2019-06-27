18ee8659d83881d0937d6085d1a14958af0f6886
olgasolov <solo_o_e@mail.ru>
2019-06-27 23:33:34 +0300

    Fix show_cards_close

--- dealer.rb
+++ dealer.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require_relative 'player'
 
 class Dealer < Player
   def show_cards_close
-    puts "#{'*' * @hand.size }"
+    puts('*' * @hand.size).to_s
   end
 end

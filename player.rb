f0c5be48e7341da0f70cc1a5bb46b62fe4b11580
olgasolov <solo_o_e@mail.ru>
2019-06-27 16:51:44 +0300

    Transfer count_points => classPlayer

--- player.rb
+++ player.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Player
-  attr_reader :bank, :hand
+  attr_reader :bank, :hand, :points
 
   def initialize
     @hand = []
+    @points = 0
   end
 
   def add_bank(bank)
@@ -20,6 +21,26 @@ class Player
       print "#{card} "
     end
     puts
+  end
+
+  def count_points
+    @hand.each do |card|
+      if card[0] =~ /[[:digit:]]/
+        if card[0] == "1"
+          @points += 10
+        else
+          @points += card[0].to_i
+        end
+      end
+
+      if card[0] =~ /[[:alpha:]]/
+        if card[0] == "A"
+          @points += 11
+        else
+          @points += 10
+        end
+      end
+    end
   end
 
   def clear_hand

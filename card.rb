3e2154ec010a315df9f9c836a588b834197c2840
olgasolov <solo_o_e@mail.ru>
2019-07-04 22:14:22 +0300

    Add determine_value, ace?, add constants

--- card.rb
+++ card.rb
@@ -1,26 +1,30 @@
 # frozen_string_literal: true
 
 class Card
-  attr_reader :rank, :suit
-
   RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
   SUITS = ['♦', '♥', '♠', '♣'].freeze
-  # VALUES = {2 => 2,
-  #          3 => 3,
-  #          4 => 4,
-  #          5 => 5,
-  #          6 => 6,
-  #          7 => 7,
-  #          8 => 8,
-  #          9 => 9,
-  #          10 => 10,
-  #          'J' => 10,
-  #          'Q' => 10,
-  #          'K' => 10,
-  #          'A' => 11}
+  ACE_VALUE = 11
+  FACE_VALUE = 10
+
+  attr_reader :rank, :suit, :value
 
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
+    @value = determine_value
+  end
+
+  def ace?
+    rank == 'A'
+  end
+
+  def determine_value
+    if ace?
+      ACE_VALUE
+    elsif %w[J Q K].include?(rank)
+      FACE_VALUE
+    else
+      rank.to_i
+    end
   end
 end

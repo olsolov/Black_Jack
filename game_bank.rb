de9fc2634515b3d0636a3d5c458d2cb81cbf0dd1
olgasolov <solo_o_e@mail.ru>
2019-07-04 22:23:37 +0300

    Fix methods

--- game_bank.rb
+++ game_bank.rb
@@ -4,17 +4,20 @@ require_relative 'bank'
 
 class GameBank < Bank
   def reward_winner(winner)
-    winner.debit(@amount)
+    winner.debit(amount)
     @amount = 0
   end
 
   def make_bets(*players)
-    players.each { |player| player.withdraw(10) }
-    @amount = 20
+    players.each do |player|
+      player.withdraw(GameRules::BET_SIZE)
+      debit(GameRules::BET_SIZE)
+    end
   end
 
   def refund(*players)
-    players.each { |player| player.debit(@amount / 2) }
+    refund_amount = amount / players.size.to_f
+    players.each { |player| player.debit(refund_amount) }
     @amount = 0
   end
 end

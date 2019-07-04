1752d22dcbede15591b997d8ffa8644245a66659
olgasolov <solo_o_e@mail.ru>
2019-07-04 22:07:34 +0300

    Add class GameRules

---
+++ game_rules.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module GameRules
+  DEALER_MAX_SUM = 17
+  HAND_MAX_SIZE = 3
+  BET_SIZE = 10
+  BJ = 21
+  ACE_ADJUSTMENT_SUM = 10
+  PLAYER_BANK_INIT_AMOUNT = 100
+end
